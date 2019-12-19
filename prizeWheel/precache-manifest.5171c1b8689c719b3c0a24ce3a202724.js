9bf58c275e7191df1164005ebdec15eeb3da44b7
Enzo1994 <120831767@qq.com>
2019-12-19 12:05:05 +0800

    写好的代码一次性提交，查看效果

---
+++ prizeWheel/precache-manifest.5171c1b8689c719b3c0a24ce3a202724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa01f1590216ade1105a0cf9e5ee4fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5041610f87f11abc5b91",
+    "url": "/static/css/main.c35c3b5a.chunk.css"
+  },
+  {
+    "revision": "068fca7b46b28bd12d8e",
+    "url": "/static/js/2.e3831b89.chunk.js"
+  },
+  {
+    "revision": "5041610f87f11abc5b91",
+    "url": "/static/js/main.a79cdc3d.chunk.js"
+  },
+  {
+    "revision": "9aa34032e74616920f36",
+    "url": "/static/js/runtime~main.cb69aa61.js"
+  },
+  {
+    "revision": "3da5638874fcece77b44dafbd51f7087",
+    "url": "/static/media/prizeWheelInnerStatic.3da56388.png"
+  },
+  {
+    "revision": "bb92e63594e1475381ce55c326efb24d",
+    "url": "/static/media/prizeWheelRoutationPart.bb92e635.png"
+  }
+]);
